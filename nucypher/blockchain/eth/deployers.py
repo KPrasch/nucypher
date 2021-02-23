64022c3051c9b5585b426d05e8f61f79cb497ac7
vzotova <vikki.zta@gmail.com>
2021-02-23 12:52:54 +0300

    Apply suggestions from code review #2549
    Co-authored-by: Derek Pierre <derek.pierre@gmail.com>
    Co-authored-by: David Núñez <david@nucypher.com>

--- nucypher/blockchain/eth/deployers.py
+++ nucypher/blockchain/eth/deployers.py
@@ -796,15 +796,15 @@ class PolicyManagerDeployer(BaseContractDeployer, UpgradeableContractMixin, Owna
                                                                          proxy_name=proxy_name)
             # If migration is happening then we should get latest StakingEscrow
             # but this contract is not yet targeted by Dispatcher
-            self.staking_library = self.blockchain.get_contract_by_name(registry=self.registry,
-                                                                        contract_name=staking_contract_name,
-                                                                        enrollment_version='latest')
+            self.staking_implementation = self.blockchain.get_contract_by_name(registry=self.registry,
+                                                                               contract_name=staking_contract_name,
+                                                                               enrollment_version='latest')
         except self.registry.UnknownContract:
             staking_contract_name = StakingEscrowDeployer.contract_name_stub
             self.staking_contract = self.blockchain.get_contract_by_name(registry=self.registry,
                                                                          contract_name=staking_contract_name,
                                                                          proxy_name=proxy_name)
-            self.staking_library = self.staking_contract
+            self.staking_implementation = self.staking_contract
 
     def check_deployment_readiness(self, *args, **kwargs) -> Tuple[bool, list]:
         staking_escrow_owner = self.staking_contract.functions.owner().call()
@@ -816,7 +816,7 @@ class PolicyManagerDeployer(BaseContractDeployer, UpgradeableContractMixin, Owna
 
     def _deploy_essential(self, contract_version: str, gas_limit: int = None, confirmations: int = 0) -> tuple:
         constructor_kwargs = {"_escrowDispatcher": self.staking_contract.address,
-                              "_escrowLibrary": self.staking_library.address}
+                              "_escrowImplementation": self.staking_implementation.address}
         policy_manager_contract, deploy_receipt = self.blockchain.deploy_contract(self.deployer_address,
                                                                                   self.registry,
                                                                                   self.contract_name,
