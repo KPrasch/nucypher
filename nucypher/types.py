ebeba59c06b6ab41fad65ba08704705d2d825af4
KPrasch <kieranprasch@gmail.com>
2024-01-13 18:19:42 +0100

    fix missing import

--- nucypher/types.py
+++ nucypher/types.py
@@ -1,7 +1,9 @@
 from typing import NewType, TypeVar
 
+from nucypher.blockchain.eth import agents
+
 ERC20UNits = NewType("ERC20UNits", int)
 NuNits = NewType("NuNits", ERC20UNits)
 TuNits = NewType("TuNits", ERC20UNits)
 
-Agent = TypeVar("Agent", bound="agents.EthereumContractAgent")
+Agent = TypeVar("Agent", bound=agents.EthereumContractAgent)
