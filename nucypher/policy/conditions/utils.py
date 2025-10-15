7c571761c343a78097541a712af71f3e81bd742a
David Núñez <david@nucypher.com>
2025-10-15 17:11:29 +0200

    No need to log condition evaluation unless it's for debugging

--- nucypher/policy/conditions/utils.py
+++ nucypher/policy/conditions/utils.py
@@ -198,7 +198,7 @@ def evaluate_condition_lingo(
     # Evaluate
     try:
         if condition_lingo:
-            log.info(f"Evaluating access conditions {condition_lingo}")
+            log.debug(f"Evaluating access conditions {condition_lingo}")
             lingo = ConditionLingo.from_dict(condition_lingo)
             result = lingo.eval(providers=providers, **context)
             if not result:
